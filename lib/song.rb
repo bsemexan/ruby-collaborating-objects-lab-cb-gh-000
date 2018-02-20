90c919fcfa6f2da97ef2b0df3b99484cbc7d9725
Berry  Semexan <bsemexan@gmail.com>
2018-02-20 21:57:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,11 @@
 class Song
   attr_accessor :name, :artist
+
+  def self.new_from_filename(filename)
+   artist_name = filename.split(" - ")[0]
+   name = filename.split(" - ")[1].gsub(".mp3", "")
+   song = self.new_by_name(name)
+   song.artist_name = artist_name
+   song
+ end
 end
