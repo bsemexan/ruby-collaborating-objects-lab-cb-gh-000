c05648dc10bd7af9a09245b8b73fac24be5101f9
Berry  Semexan <bsemexan@gmail.com>
2018-02-20 22:47:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
   def self.new_by_filename(name)
     file = name.split(" - ")
     song = self.new.file[0]
-    artist = Artist.new.arr[1]
+    artist = Artist.new.file[1]
 
     file
  end
