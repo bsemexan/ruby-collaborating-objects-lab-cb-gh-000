46163818a6c598fe773395a33f59b0a0b72ed40b
Berry  Semexan <bsemexan@gmail.com>
2018-02-20 23:34:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,16 +8,13 @@ class Song
   def artist_name=(name)
 
   end
-
-  def self.new_by_filename(name)
-    file = name.split(" - ")
-    song = self.new.file[0]
-    artist = Artist.new.file[1]
-    artits.songs << song
-    song.artist = artist
-    artist.save
-    song
- end
+  
+  def self.new_by_filename(filename)
+      artist, song = filename.split(" - ")
+      new_song = self.new(song)
+      new_song.artist_name = artist
+      new_song
+    end
 
  def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
