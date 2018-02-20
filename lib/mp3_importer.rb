e9bf9dfaafe9535f202b632d9a3d4e4e34ccf50c
Berry  Semexan <bsemexan@gmail.com>
2018-02-20 23:40:38 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,8 +6,10 @@ class MP3Importer
   end
 
   def files
-  end
+   @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+ end
 
-  def import
-  end
+ def import
+   files.each{|f| Song.new_by_filename(f)}
+ end
 end
