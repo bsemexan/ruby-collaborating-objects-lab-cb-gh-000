4d04f1f8cbb8fbbd2de76a4ffa7865bf3ac57c98
Berry  Semexan <bsemexan@gmail.com>
2018-02-20 23:57:37 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+
+  attr_accessor :songs, :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.find_or_create_by_name(name)
+    @@all.find { |artist| artist.name == name } || self.new(name).tap {|a| a.save}
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    @songs.each { |song| puts song.name }
+  end
+end
