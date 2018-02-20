7cc1dedbdb574e22d9178317d585963fb3539696
Berry  Semexan <bsemexan@gmail.com>
2018-02-20 23:27:09 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,6 +11,10 @@ class Artist
     @songs << song
   end
 
+  def self.find(name)
+    @@all.detect{ |song| song.name == name }
+  end
+
   def self.find_or_create_by_name(name)
     @@all.index { |artist| artist.name == name } || self.new(name)
   end
